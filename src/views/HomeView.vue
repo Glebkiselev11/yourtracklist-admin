edc3c3353a6238420f269725ee29a6e00a363960
Gleb Kiselev <g.kiselev@examus.net>
2022-04-27 21:46:04 +0300

    Remove extra code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <button @click="increment()" v-text="'increase counter'" />
+    <div>{{ counter }}</div>    
   </main>
 </template>
+
+<script setup lang="ts">
+import { useCounterStore } from "@/stores/counter";
+import {storeToRefs} from "pinia"
+const { counter } = storeToRefs(useCounterStore());
+const { increment } = useCounterStore();
+</script>
+
+<style scoped lang="scss">
+main {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
