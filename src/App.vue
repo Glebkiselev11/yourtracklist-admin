3307c1b8735b7765b549a0ac01a73980403b78df
Gleb Kiselev <g.kiselev@examus.net>
2022-04-28 23:22:06 +0300

    Add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <template v-slot:append>
+        <v-btn color="black" plain variant="contained-text">Load release</v-btn>
+      </template>
+    </v-app-bar>
+
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
+</template>
+
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 </script>
-<template>
-  <RouterView />
-</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,13 @@
 <template>
-  <main>
-    <button @click="increment()" v-text="'increase counter'" />
-    <div>{{ counter }}</div>    
-  </main>
+  <div class="d-flex justify-center align-center">
+    <v-btn @click="increment()" v-text="'increase counter'" />
+    <div>{{ counter }}</div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useCounterStore } from "@/stores/counter";
-import {storeToRefs} from "pinia"
+import { storeToRefs } from "pinia";
 const { counter } = storeToRefs(useCounterStore());
 const { increment } = useCounterStore();
 </script>
-
-<style scoped lang="scss">
-main {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
