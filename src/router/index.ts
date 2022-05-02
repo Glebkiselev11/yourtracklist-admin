9be5d02a8c91987a893b44763e90fa5e9079428a
Gleb Kiselev <g.kiselev@examus.net>
2022-05-02 23:52:46 +0300

    Rename LoadView with LoadReleaseView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LoadView from "../views/LoadView.vue";
+import LoadReleaseView from "../views/LoadReleaseView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: LoadView,
+      component: LoadReleaseView,
     },
   ],
 });
