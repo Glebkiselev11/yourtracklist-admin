8032f00b8b9a627663c9e7f314b72adab0486bfe
Gleb Kiselev <g.kiselev@examus.net>
2022-05-03 23:17:49 +0300

    Move clien project in self folder

---
+++ admin-client/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <AppHeader />
+
+    <v-main class="bg-grey-lighten-4">
+      <RouterView />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import AppHeader from "@/components/AppHeader.vue";
+</script>
---
+++ admin-client/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar elevation="2">
+    <template v-slot:prepend>
+      <v-app-bar-title class="title font-weight-bold">
+        Yourtracklist
+      </v-app-bar-title>
+
+      <div class="ml-4"><img src="@/assets/logo.png" alt="logo" /></div>
+    </template>
+    <template v-slot:append>
+      <v-btn color="black" variant="contained-text">Load release</v-btn>
+    </template>
+  </v-app-bar>
+</template>
+
+<style scope lang="scss">
+.title {
+  font-size: 28px !important;
+}
+</style>
