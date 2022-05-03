697b4b90f531e6ee34e9ec52db2b38277b357f67
Gleb Kiselev <12bny23gleb@gmail.com>
2022-05-03 23:46:25 +0300

    Add simple actix api

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Yourtracklist v2 server")
+}
+
+#[post("/create")]
+async fn create(body: web::Json<NewRelease>) -> String {
+    format!("{}, {}", body.author, body.name)
+}
+
+#[derive(Deserialize)]
+struct NewRelease {
+    name: String,
+    author: String,
 }
