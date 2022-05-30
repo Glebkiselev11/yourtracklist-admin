c2706a3c4f28f5c2f5411e467bd1d634d9af3694
Gleb Kiselev <12bny23gleb@gmail.com>
2022-05-30 22:19:01 +0300

    Add common class for config

--- admin-client/src/common/config.ts
+++ admin-client/src/common/config.ts
@@ -1,23 +1,18 @@
-export const AcceptableCoverFormats = {
-    list: ["image/jpeg", "image/png"],
+export class Config {
+    list: Array<string>;
+
+    constructor(list: Array<string>) {
+        this.list = list;
+    }
 
     contain(type: string): boolean {
         return this.list.includes(type);
-    },
+    }
 
     get(): string {
         return this.list.join(",");
-    },
-};
+    }
+}
 
-export const AcceptableAudioTracksFormats = {
-    list: ["audio/mpeg"],
-
-    contain(type: string): boolean {
-        return this.list.includes(type);
-    },
-
-    get(): string {
-        return this.list.join(",");
-    },
-};
+export const AcceptableCoverFormats = new Config(["image/jpeg", "image/png"]);
+export const AcceptableAudioTracksFormats = new Config(["audio/mpeg"]);
